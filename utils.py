4caeb7852e51f8a432f0912213b57c5b15c34cd5
Mussa Niyodusenga <mussa.niyodusenga@thrypes.com>
2025-05-30 00:07:57 +0800

    added utilits file

---
+++ utils.py
@@ -0,0 +1,16 @@
+def calculate_iou(box1, box2):
+    x1 = max(box1[0], box2[0])
+    y1 = max(box1[1], box2[1])
+    x2 = min(box1[2], box2[2])
+    y2 = min(box1[3], box2[3])
+    intersection = max(0, x2 - x1) * max(0, y2 - y1)
+    area1 = (box1[2] - box1[0]) * (box1[3] - box1[1])
+    area2 = (box2[2] - box2[0]) * (box2[3] - box2[1])
+    union = area1 + area2 - intersection
+    return intersection / union if union != 0 else 0
+
+def is_new_detection(class_name, new_box, detected_boxes, iou_threshold=0.5):
+    for old_box in detected_boxes[class_name]:
+        if calculate_iou(old_box, new_box) > iou_threshold:
+            return False
+    return True
